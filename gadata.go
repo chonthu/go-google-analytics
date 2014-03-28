ae47a271dc33bc454efea9b3b85b7fd74d57f595
Val Lyashov <val@plstr.com>
2014-03-29 10:43:33 +1100

    import func

--- gadata.go
+++ gadata.go
@@ -1,4 +1,33 @@
 package gadata
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
 type AuthInfo struct {
+	ClientID     string   `json:client_id`
+	ClienSecret  string   `json:client_secret`
+	AuthURI      string   `json:auth_uri`
+	TokenURI     string   `json:token_uri`
+	RedirectURIs []string `json:redirect_uris`
+}
+
+// ImportConfig imports client secret information from the JSON obtained
+// from Google Developer Console (API console).
+func ImportConfig(filename string) (conf *AuthInfo, err error) {
+	config := new(AuthInfo)
+	configFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error opening config file: %s\n", filename)
+	}
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&config); err != nil {
+		log.Fatalf("Error encoding config struct", err.Error())
+	}
+
+	fmt.Println(config.ClienSecret)
+	return
 }
