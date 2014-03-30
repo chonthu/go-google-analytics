abb16c9676d45c3c1f8dd7cb585d31f7b9696142
Val Lyashov <val@plstr.com>
2014-03-30 22:50:56 +1100

    basic authentication functionality

--- gadata_test.go
+++ gadata_test.go
@@ -9,10 +9,29 @@ import (
 func TestImportConfig(t *testing.T) {
 	authFile := "client_secret.json"
 	conf, err := ImportConfig(authFile)
-	if conf != nil {
+	if conf == nil {
 		t.Errorf("No data read from %s", authFile)
 	}
 	if err != nil {
 		t.Errorf("Error reading file %s", authFile)
 	}
+}
+
+// TestDBGetTokens validates token retrieval from
+// default local store (.dat file).
+func TestDBGetTokens(t *testing.T) {
+	tokens, err := DBGetTokens()
+	if err != nil {
+		t.Errorf("Error retrieving tokens from DB")
+	} else if tokens == nil {
+		t.Errorf("No token data imported from DB")
+	}
+
+}
+
+// TestInitConnection initialises gadata object
+// and writes out tokens to local dat file if
+// one doesn't exist.
+func TestInitConnection(t *testing.T) {
+	InitConnection()
 }
